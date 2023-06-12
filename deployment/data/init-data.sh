6f19e78af95e1f833c3c4ca0924c11e73cfc3c02
dabico <dabico@usi.ch>
2023-06-12 10:09:03 +0200

    Updated some documentation

--- deployment/data/init-data.sh
+++ deployment/data/init-data.sh
@@ -3,11 +3,9 @@
 # TODO: By default, these files are completely empty.
 #       If you want to specify the starting labels and reviewers,
 #       you must edit the two respective text files.
-#       Keep in mind that these two files should be newline separated.
-#       Furthermore, we recommend all data be in lower-cased.
-#       To import instances, you must first decide on how and when they should be loaded.
-#       Either add them to the container using a bind mount,
-#       or do so at build time (refer to the `server` Dockerfile for more details).
+#       Keep in mind that entries should be newline separated, and lower-cased.
+#       To import instances, you must first decide how and when the files should be added.
+#       Either add them to the container using a bind mount, or do so on image build.
 #       After that, use a \COPY command similar to the ones shown in the file.
 
 set -e
