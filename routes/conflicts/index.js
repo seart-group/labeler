7991e8f0dfdd502439f97775916435e7256b9df2
dabico <dabico@usi.ch>
2023-06-12 09:41:11 +0200

    Instance and Conflict pages now display category for each item

--- routes/conflicts/index.js
+++ routes/conflicts/index.js
@@ -5,11 +5,9 @@ export const get = async (req, res) => {
     const limit = req.query.limit || 10;
     const id = req.query.id || 1;
     const order = (id > 0) ? "ASC" : "DESC";
-    const query = `SELECT id FROM instance_review_conflict ORDER BY id ${order} OFFSET $1 LIMIT $2`;
+    const query = `SELECT * FROM instance_review_conflict ORDER BY id ${order} OFFSET $1 LIMIT $2`;
     const { rows: conflicts } = await pool.query(query, [ (current - 1) * limit, limit ]);
-    const { rows: [ { count: items } ] } = await pool.query(
-        "SELECT COUNT(id) FROM instance_review_conflict"
-    );
+    const { rows: [ { count: items } ] } = await pool.query("SELECT COUNT(id) FROM instance_review_conflict");
     const pages = Math.ceil(items / req.query.limit);
     res.render("conflicts", {
         conflicts,
--- routes/instances/index.js
+++ routes/instances/index.js
@@ -5,7 +5,7 @@ export const get = async (req, res) => {
     const limit = req.query.limit || 10;
     const id = req.query.id || 1;
     const order = (id > 0) ? "ASC" : "DESC";
-    const query = `SELECT id FROM instance ORDER BY id ${order} OFFSET $1 LIMIT $2`;
+    const query = `SELECT * FROM instance ORDER BY id ${order} OFFSET $1 LIMIT $2`;
     const { rows: instances } = await pool.query(query, [ (current - 1) * limit, limit ]);
     const { rows: [ { count: items } ] } = await pool.query("SELECT COUNT(id) FROM instance");
     const pages = Math.ceil(items / req.query.limit);
