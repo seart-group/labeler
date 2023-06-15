638ba0b09e05560c4a082f27fef901b79654a935
dabico <dabico@usi.ch>
2023-06-15 11:28:16 +0200

    Updating `/progress` view to incorporate the usage of new views

--- routes/progress/index.js
+++ routes/progress/index.js
@@ -1,7 +1,7 @@
 import pool from "../../util/pg-pool.js";
 
 export const get = async (_, res) => {
-    const { rows: reviewers } = await pool.query("SELECT * FROM instance_review_progress");
+    const { rows: reviewers } = await pool.query("SELECT * FROM reviewer_progress");
     const { rows: buckets } = await pool.query("SELECT * FROM instance_review_bucket");
     res.render("progress", { reviewers, buckets });
 };
