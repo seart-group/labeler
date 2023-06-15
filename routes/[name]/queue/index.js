39cd5d66384227bb933fe89f2444858142a9c065
dabico <dabico@usi.ch>
2023-06-15 11:31:19 +0200

    Updating review queue page header to include the number of discards

--- routes/[name]/queue/index.js
+++ routes/[name]/queue/index.js
@@ -3,7 +3,7 @@ import pool from "../../../util/pg-pool.js";
 export const get = async (req, res) => {
     const { rows: labels } = await pool.query("SELECT * FROM label ORDER BY name");
     const { rows: [ reviewer ] } = await pool.query(
-        "SELECT * FROM instance_review_progress WHERE name = $1 LIMIT 1",
+        "SELECT * FROM reviewer_progress WHERE name = $1 LIMIT 1",
         [ req.params.name ]
     );
     const { rows: [ instance ] } = await pool.query(
