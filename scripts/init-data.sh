c824b9df3a5bfae821c84aa2293579041b76fcd1
dabico <dabico@usi.ch>
2024-01-26 14:43:25 +0100

    Re-contextualise the init-data.sh script to accept a directory parameter

--- scripts/init-data.sh
+++ scripts/init-data.sh
@@ -9,20 +9,27 @@
 
 set -e
 
+DATA_DIRECTORY=${DATA_DIRECTORY:-$PWD}
+DATA_LABEL="$DATA_DIRECTORY/label.txt"
+DATA_REVIEWER="$DATA_DIRECTORY/reviewer.txt"
+DATA_INSTANCE="$DATA_DIRECTORY/instance"
+DATA_INSTANCE_CSV="$DATA_INSTANCE.csv"
+DATA_INSTANCE_TSV="$DATA_INSTANCE.tsv"
+
 function exec_cmd() {
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "$1"
 }
 
-if [ -f "/docker-entrypoint-initdb.d/label.txt" ];
-then exec_cmd "\COPY label(name) FROM '/docker-entrypoint-initdb.d/label.txt';";
+if [ -f "$DATA_LABEL" ];
+then exec_cmd "\COPY label(name) FROM '$DATA_LABEL';";
 fi
 
-if [ -f "/docker-entrypoint-initdb.d/reviewer.txt" ];
-then exec_cmd "\COPY reviewer(name) FROM '/docker-entrypoint-initdb.d/reviewer.txt';";
+if [ -f "$DATA_REVIEWER" ];
+then exec_cmd "\COPY reviewer(name) FROM '$DATA_REVIEWER';";
 fi
 
-if [ -f "/docker-entrypoint-initdb.d/instance.csv" ];
-then exec_cmd "\COPY instance(category, data) FROM '/docker-entrypoint-initdb.d/instance.csv' DELIMITER E',' QUOTE E'\b' CSV;";
-elif [ -f "/docker-entrypoint-initdb.d/instance.tsv" ];
-then exec_cmd "\COPY instance(category, data) FROM '/docker-entrypoint-initdb.d/instance.tsv' DELIMITER E'\t' QUOTE E'\b' CSV;";
+if [ -f "$DATA_INSTANCE_CSV" ];
+then exec_cmd "\COPY instance(category, data) FROM '$DATA_INSTANCE_CSV' DELIMITER E',' QUOTE E'\b' CSV;";
+elif [ -f "$DATA_INSTANCE_TSV" ];
+then exec_cmd "\COPY instance(category, data) FROM '$DATA_INSTANCE_TSV' DELIMITER E'\t' QUOTE E'\b' CSV;";
 fi
