7bd09b9eb7febd1b5e53aeb4c05b936e2795c48a
dabico <dabico@usi.ch>
2024-04-09 00:04:44 +0200

    Replace `eslintrc` with new flat config file

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+    js.configs.recommended,
+    {
+        files: [
+            "**/*.js",
+            "**/*.ts",
+            "**/*.cjs",
+            "**/*.mjs",
+        ],
+        languageOptions: {
+            sourceType: "module",
+            ecmaVersion: 2024,
+            globals: {
+                ...globals.node,
+                ...globals.es2021,
+                ...globals.browser,
+            }
+        },
+        rules: {
+            "semi": ["error", "always"],
+            "indent": ["error", 4],
+            "quotes": ["error", "double"],
+            "linebreak-style": ["error", "unix"],
+            "no-unused-vars": ["error" , {
+                "argsIgnorePattern": "^_",
+                "varsIgnorePattern": "^_",
+                "caughtErrorsIgnorePattern": "^_",
+            }],
+        }
+    },
+];
